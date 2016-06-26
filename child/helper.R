15f7c8dae7f32c9aa7d8db446554a13712f2b616
Keith Hughitt <keith.hughitt@gmail.com>
2016-06-26 13:08:00 -0400

    Bunch of updates; switched from using UTR length and stats output from elsayed-lab/gene-structure-determination (considering adjacent pairs of UTRs simultaneously

--- child/helper.R
+++ child/helper.R
@@ -37,3 +37,102 @@ gene_structure_summary <- function(utr5_lengths, cds_lengths, utr3_lengths) {
     
     return(summary_dat)
 }
+
+#
+# get_intergenic_lengths
+#
+get_intergenic_lengths <- function(gr) {
+    # Vector to keep track of intergenic lengths
+    result <- data.frame()
+
+    # Make sure the GenomicRanges instance is in sorted order
+    gr <- sortSeqlevels(gr)
+    gr <- sort(gr, ignore.strand=TRUE)
+
+    # Iterate over chromosome
+    for (ch in levels(seqnames(gr))) {
+        # Get GRange subsets for the chromosome
+        gr_ch <- gr[seqnames(gr) == ch]
+
+        # Iterate over features on chromosome
+        # we can skip the last feature since it cannot fall between two genes
+        # on the same strand
+        for (i in 1:(length(gr_ch) - 1)) {
+            feature <- gr_ch[i]
+            next_feature <- gr_ch[i + 1]
+
+            # Positive strand (3'UTR followed by 5'UTR)
+            if (as.logical(strand(feature) == '+' & feature$type == 'three_prime_UTR')) {
+                # If next feature is a 5'UTR on the same strand, then use it
+                # to determine the intergenic region length
+                if (as.logical(strand(next_feature) == '+' &  
+                               next_feature$type == 'five_prime_UTR')) {
+                    intergenic_length <- start(next_feature) - end(feature)
+                    result <- rbind(result, c(feature$ID, next_feature$ID, intergenic_length))
+                    #message(sprintf("%s (i=%d): %d", ch, i, intergenic_length))
+                }
+            } else if (as.logical(strand(feature) == '-' & feature$type == 'five_prime_UTR')) {
+                # Negative strand (5'UTR followed by 3'UTR)
+
+                # If next feature is a 3'UTR on the same strand, then use it
+                # to determine the intergenic region length
+                if (as.logical(strand(next_feature) == '-' &  
+                               next_feature$type == 'three_prime_UTR')) {
+                    intergenic_length <- start(next_feature) - end(feature)
+                    result <- rbind(result, c(feature$ID, next_feature$ID, intergenic_length))
+                    message(sprintf("%s (i=%d): %d", ch, i, intergenic_length))
+                }
+            }
+        }
+    }
+    colnames(result) <- c("left", "right", "length")
+    result <- as.numeric(result$length)
+
+    return(result)
+}
+
+#
+# get_intercds_sequences
+#
+# Finds the inter-CDS sequence for each pair of adjacent CDS's on the same
+# strand
+#
+get_intercds_sequences <- function(gr, fasta) {
+    # Vector to keep track of inter-CDS sequences
+    result <- c()
+
+    # Make sure the GenomicRanges instance is in sorted order
+    gr <- sortSeqlevels(gr)
+    gr <- sort(gr)
+
+    # Iterate over chromosome
+    for (ch in levels(seqnames(gr))) {
+        # Get GRange subsets for the chromosome
+        gr_ch <- gr[seqnames(gr) == ch]
+
+        gr_cds <- gr_ch[gr_ch$type == 'CDS']
+
+        # Iterate over CDS's on chromosome
+        # we can skip the last feature since it cannot fall between two genes
+        # on the same strand
+        for (i in 1:(length(gr_cds) - 1)) {
+            cds <- gr_cds[i]
+            next_cds <- gr_cds[i + 1]
+
+            if (strand(cds) == strand(next_cds)) {
+                result <- append(result, subseq(fasta[ch], end(cds), start(next_cds)))
+            }
+        }
+    }
+
+    return(result)
+}
+
+
+#
+# detect_polypyrimidine_tracts
+#
+detect_polypyrimidine_tracts <- function() {
+
+}
+
