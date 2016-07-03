28ca40bfdc1779f26b30212bf060ecf50df42ecc
Keith Hughitt <keith.hughitt@gmail.com>
2016-07-03 12:27:07 -0400

    Added some helper scripts to generate various mapping statistics for T. cruzi; continuing work on gene structure description

---
+++ scripts/tcruzi_ystrain/S01_count_feature_reads.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/zsh
+
+#
+# Counts the total number of SL-containing or Poly(A)-containing reads 
+#
+sl_outfile="../../input/tcruzi_ystrain/S01_num_reads_sl.csv"
+polya_outfile="../../input/tcruzi_ystrain/S01_num_reads_polya.csv"
+echo "sample_id,num_reads_sl" > $sl_outfile 
+echo "sample_id,num_reads_polya" > $polya_outfile 
+
+# SL reads
+for x in $SCRATCH/utr_analysis/tcruzi_ystrain/tcruzi-infecting-hsapiens-*/input/HPGL0*; do
+	sample_id=$(echo $x | egrep -o "HPGL[0-9]+" | head -1)
+
+    # SL
+    infiles=($SCRATCH/utr_analysis/tcruzi_ystrain/*/*spliced_leader/minlength6_mindiff-3/${sample_id}/results/matched_reads_*.csv)
+    num_reads=$(wc -l $infiles | tail -1 | egrep -o "[0-9]+")
+    echo $num_reads
+	printf "%s,%d\n" $sample_id $num_reads >> $sl_outfile
+
+    # Poly(A)
+    infiles=($SCRATCH/utr_analysis/tcruzi_ystrain/*/poly*/minlength6_mindiff-3/${sample_id}/results/matched_reads_*.csv)
+    num_reads=$(wc -l $infiles | tail -1 | egrep -o "[0-9]+")
+    echo $num_reads
+	printf "%s,%d\n" $sample_id $num_reads >> $polya_outfile
+done
+
